15236ae152f61e86abc877b44667c7afc2550277
Ewa-K <ewa-1988@yahoo.com>
2018-06-11 13:07:48 +0200

    Change app

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 var Counter = React.createClass({
 
+    getDefaultProps() {
+        console.log('getDefaultProps, dzięki niej można ustawić domyślne wartości propsów');
+    },
+
+    getInitialState() {
+        console.log('getInitialState, dzięki niej ustalamy początkowy stan elementu');
+    },
+
+    componentWillMount() {
+        console.log('componentWillMount możemy wykorzystać, kiedy na ekranie chcemy zobaczyć stan początkowy elementu.');
+    },
+
+    componentDidMount() {
+        console.log('componentDidMount możemy wykorzystać do aktualizacji stanu elementu np. w wyniku wywołania AJAX.');
+    },
+
+    componentWillReceiveProps() {
+        console.log('componentWillReceiveProps możemy wykorzystać do sprawdzenia czy odświeżanie nastąpiło w wyniku zmiany obiektu this.props.');
+    },
+
+    shouldComponentUpdate() {
+        console.log('shouldComponentUpdate, dzięki niemu możemy np. anulować kolejne metody cyklu życia komponentu.');
+        return true;
+    },
+
+    componentWillUpdate() {
+        console.log('componentWillUpdate wywołujemy po to, aby przygotować się na nadchodzące zmiany');
+    },
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate, dzięki niemu możemy aktualizować stan komponentu.');
+    },
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount służy do wyczyszczenia pozostałości po działaniu komponentu');
+    },
+
     getInitialState: function() {
         return {
             counter: 0,
